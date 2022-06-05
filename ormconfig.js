0499a16430f4d7e4bda475edca6db3458e60868e
TrongPhuoc001 <85543424+TrongPhuoc001@users.noreply.github.com>
2022-06-05 19:32:49 +0700

    update dbconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,7 @@
 module.exports = {
     type: 'postgres',
-    url: process.env.DATABASE_URL,
-    port: 5432,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL|| process.env.DB_URL,
+
     entities: [
         process.env.NODE_ENV === 'test'
             ? 'src/**/*.entity.ts'
